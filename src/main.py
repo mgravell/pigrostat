56c26bc907320b41d77b5cbed9b9594090c2bdf7
Marc Gravell <marc.gravell@gmail.com>
2023-12-07 12:01:17 +0000

    don't hard-code LED pin (different between boards)

--- src/main.py
+++ src/main.py
@@ -2,117 +2,137 @@ import time, sht30, ujson
 from machine import I2C, SoftI2C, Pin, ADC
 from ssd1306 import SSD1306_I2C
 
-print("Loading configuration...")
-f = open('config.json', 'r')
-config = ujson.loads(f.read())
-f.close()
+try:
+    print("Loading configuration...")
+    f = open('config.json', 'r')
+    config = ujson.loads(f.read())
+    f.close()
 
-onboardLED = Pin(25, Pin.OUT)
+    class DummyPin:
+        def on(self):
+            pass
+        def off(self):
+            pass
+        def value(self):
+            return 0
+        def value(self, val):
+            pass
 
-class DebugDisplay:
-    def fill(self, x):
-        pass # no-op
+    try:
+        led = machine.Pin.board.LED
+    except:
+        led = DummyPin()
         
-    def show(self, ):
-        pass # no-op
+    class DebugDisplay:
+        def fill(self, x):
+            pass # no-op
 
-    def text(self, msg, x, y):
-        print(msg)
+        def show(self):
+            pass # no-op
 
-display = DebugDisplay()
+        def text(self, msg, x, y):
+            print(msg)
 
-if 'display' in config:
-    print("Configuring display...")
-    cfg = config["display"]
-    i2c_display=I2C(0, sda=Pin(cfg["sda"]), scl=Pin(cfg["scl"]), freq=cfg["freq"])
-    time.sleep(1) # allow things to initialize
-    print("Scanning for display I2C...")
-    devices = i2c_display.scan()
-    if len(devices) == 1:
-        print(f'Discovered at {devices[0]}')
-        display = SSD1306_I2C(cfg.width, cfg.height, i2c_display)
-    else:
-        print('Display is configured but was not found')
-else:
-    print('Display not configured')
+    display = DebugDisplay()
 
-sensor = config["sensor"]
-print(sensor)
-values = sensor["values"]
-print(f'Sensor has {len(values)} output values')
-for val in values:
-    if 'relay' in val:
-        if val["relay"] is None:
-            val.pop("relay")
+    if 'display' in config:
+        print("Configuring display...")
+        cfg = config["display"]
+        i2c_display=I2C(0, sda=Pin(cfg["sda"]), scl=Pin(cfg["scl"]), freq=cfg["freq"])
+        time.sleep(1) # allow things to initialize
+        print("Scanning for display I2C...")
+        devices = i2c_display.scan()
+        if len(devices) == 1:
+            print(f'Discovered at {hex(devices[0])}')
+            display = SSD1306_I2C(cfg["width"], cfg["height"], i2c_display)
         else:
-            val["relay"] = Pin(val["relay"], Pin.OUT)
+            print('Display is configured but was not found')
+    else:
+        print('Display not configured')
 
-print("Configuring sensor...")
-i2c_sensor=SoftI2C(sda=Pin(sensor["sda"]), scl=Pin(sensor["scl"]))
-print("Scanning for sensor...")
-devices = i2c_sensor.scan()
-sht = None
-if sensor["addr"] in devices:
-    print(f'Sensor detected, configuring...')
-    sht=sht30.SHT30(i2c=i2c_sensor, i2c_address=sensor["addr"])
-else:
-    print('No sensor detected with specified address')
-
-cpu = machine.ADC(4) # allows access to CPU temperature
-
-# main loop (note: no point running if we don't have a sensor)
-print('Running...')
-while sht is not None:
-    # enable the device LED to show we're alive
-    onboardLED.value(1)
-    
-    display.fill(0) # wipe and redraw
-    
-    # read the values from the sensor
-    tuple = sht.measure()
-    for idx, x in enumerate(values):
-        val = tuple[idx]
-        display.text(f'{x["label"]}: {round(val, 1)} {x["unit"]}', 0, idx * 12)
-        
-        if 'relay' in x:
-            relay = x["relay"]
-            current = relay.value()
-            target = current # assume no change
-
-            # logic here is absurdly simple latch; we don't need to be clever
-            if current >= 0.9: # treat as on
-                if val >= x["off"]:
-                    target = 0
+    sensor = config["sensor"]
+    print(sensor)
+    values = sensor["values"]
+    print(f'Sensor has {len(values)} output values')
+    for val in values:
+        if 'relay' in val:
+            if val["relay"] is None:
+                val.pop("relay")
             else:
-                if val <= x["on"]:
-                    target = 1
-                    
-            if target != current:
-                relay.value(target)
-                print(f'{x["name"]} now {relay.value()}')
-    
-    # read the ambient CPU temperature (ADC 4 is a slope showing temp,
-    # with defined gradient/origin; these numbers are from the spec)
-    # see: https://electrocredible.com/raspberry-pi-pico-temperature-sensor-tutorial/
-    ADC_voltage = cpu.read_u16() * (3.3 / (65536))
-    cputemp = 27 - (ADC_voltage - 0.706) / 0.001721
-    
-    display.text(f'CPU: {round(cputemp, 1)} C', 0, 24)
-    display.show()
-    
-    # pause for long enough to ensure the status LED is visible
-    time.sleep(0.05)
-    
-    # show that we're done thinking (thinking will appear as a flash)
-    onboardLED.value(0)
-    
-    # wait a second (there's no point updating too frequently)
-    time.sleep(config["delay"])
+                val["relay"] = Pin(val["relay"], Pin.OUT)
 
-# show exit condition
-print('Exit')
-for i in range(5):
-    onboardLED.value(1)
-    time.sleep(0.1)
-    onboardLED.value(0)
-    time.sleep(0.1)
+    print("Configuring sensor...")
+    i2c_sensor=SoftI2C(sda=Pin(sensor["sda"]), scl=Pin(sensor["scl"]))
+    print("Scanning for sensor...")
+    devices = i2c_sensor.scan()
+    sht = None
+    if sensor["addr"] in devices:
+        print(f'Sensor detected, configuring...')
+        sht=sht30.SHT30(i2c=i2c_sensor, i2c_address=sensor["addr"])
+    else:
+        print('No sensor detected with specified address')
+
+    cpu = machine.ADC(4) # allows access to CPU temperature
+
+    # main loop (note: no point running if we don't have a sensor)
+    print('Running...')
+    while sht is not None:
+        # enable the device LED to show we're alive
+        led.on(1)
+
+        display.fill(0) # wipe and redraw
+
+        # read the values from the sensor
+        tuple = sht.measure()
+        for idx, x in enumerate(values):
+            val = tuple[idx]
+            display.text(f'{x["label"]}: {round(val, 1)} {x["unit"]}', 0, idx * 12)
+
+            if 'relay' in x:
+                relay = x["relay"]
+                current = relay.value()
+                target = current # assume no change
+
+                # logic here is absurdly simple latch; we don't need to be clever
+                if current >= 0.9: # treat as on
+                    if val >= x["off"]:
+                        target = 0
+                else:
+                    if val <= x["on"]:
+                        target = 1
+
+                if target != current:
+                    relay.value(target)
+                    print(f'{x["name"]} now {relay.value()}')
+
+        # read the ambient CPU temperature (ADC 4 is a slope showing temp,
+        # with defined gradient/origin; these numbers are from the spec)
+        # see: https://electrocredible.com/raspberry-pi-pico-temperature-sensor-tutorial/
+        ADC_voltage = cpu.read_u16() * (3.3 / (65536))
+        cputemp = 27 - (ADC_voltage - 0.706) / 0.001721
+
+        display.text(f'CPU: {round(cputemp, 1)} C', 0, 24)
+        display.show()
+
+        # pause for long enough to ensure the status LED is visible
+        time.sleep(0.05)
+
+        # show that we're done thinking (thinking will appear as a flash)
+        led.off()
+
+        # wait a second (there's no point updating too frequently)
+        time.sleep(config["delay"])
+finally:
+    # show exit condition
+    try:
+        display.fill(0) # wipe and redraw
+        display.text('Exited', 0, 0)
+        display.show()
+    except:
+        pass
+
+    for i in range(5):
+        led.on()
+        time.sleep(0.1)
+        led.off()
+        time.sleep(0.1)
